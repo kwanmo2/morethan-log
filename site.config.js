6c161f6c47455879d75a623d9f2a83d7d91341bf
jamesyeon <kwanmo2@gmail.com>
2025-07-20 01:58:31 +0900

    test
    git push

--- site.config.js
+++ site.config.js
@@ -11,12 +11,12 @@ const CONFIG = {
     instagram: "",
   },
   projects: [
-    /*
+    
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `None`,
+      href: "",
     },
-    */
+    
   ],
   // blog setting (required)
   blog: {
