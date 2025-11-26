9b647897b08c3204f00b4df60e6650540767e27c
jamesyeon <kmyeon@envision.co.kr>
2025-11-27 00:38:16 +0900

    turn on robot.txt and sitemap

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   images: {
     domains: [
-      'www.notion.so',
-      'lh3.googleusercontent.com',
-      'lh4.googleusercontent.com',
-      'lh5.googleusercontent.com',
-      'lh6.googleusercontent.com',
-      's3-us-west-2.amazonaws.com',
+      'slowbeam.vercel.app/',
+      'slowbeam.dev',
     ],
   },
 }
