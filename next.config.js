49b1462dfc1ca8dc69d193f312066731fc1e55c3
James Yeon <kwanmo2@gmail.com>
2025-11-27 11:20:02 +0900

    Allow thumbnails from remote image hosts

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   images: {
     domains: [
-      'slowbeam.dev',
-      'slowbeam.vercel.app',
+      "slowbeam.dev",
+      "slowbeam.vercel.app",
+      "www.notion.so",
+      "images.unsplash.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
   },
 }
